8e4a8ab5dd86c0b2f2e37221a61f07ce24739df0
acr <hello@crucifixarnaud.com>
2017-02-27 14:56:19 +0100

    Fix indentation

--- app/modules/users/user.middlewares.js
+++ app/modules/users/user.middlewares.js
@@ -13,7 +13,7 @@ function isLoggedIn(req, res, next) {
 
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated())
-      return next();
+    return next();
 
   // if they aren't redirect them to the home page
   res.redirect('/');
