3b9041a23b856ca692df324e5b1fa4cb09a79641
acr <hello@crucifixarnaud.com>
2017-07-26 19:45:29 +0200

    Display admin/artist list using react

--- app/assets/js/admin.js
+++ app/assets/js/admin.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Request from 'request';
+import moment from 'moment';
 
 /**
  * Admin
@@ -10,14 +11,14 @@ export default class Admin extends React.Component {
     super(props);
 
     this.state = {
-      artist: {}
+      artists: []
     };
   }
 
   componentWillMount() {
-    const artistsUrl = window.location.href + 'artists/geojson';
+    const artistsUrl = window.location.origin + '/artists';
 
-    let artistsPromise = new Promise(function(resolve, reject) {
+    let artistsPromise = new Promise(function(resolve) {
       Request(artistsUrl, (error, response, body) => {
         if (!error && response.statusCode === 200) {
           // Success
@@ -30,14 +31,63 @@ export default class Admin extends React.Component {
 
     artistsPromise.then((res) => {
       this.setState({
-        artists: res.artists,
-        artistsTotal: res.artists.length,
+        artists: res,
+        artistsTotal: res.length,
       });
-      this.createAtlas(res);
     });
   }
 
   render() {
+
+
+    // Artists row
+    const artistsResult = this.state.artists.map((artist, step) => {
+      // Artist Bio Url
+      let artistBioUrl;
+      if (artist.bio.url) {
+        artistBioUrl = (
+          <a href={artist.bio.url} title="View details (json)">{artist.name}</a>
+        );
+      } else {
+        artistBioUrl = (
+          <p>{artist.name}</p>
+        );
+      }
+
+      const artistCategories = artist.categories.map((category, step) =>
+        <span key={artist._id+step}>{category}&nbsp;</span>
+      );
+
+      return (
+        <tr className='table__row' key={artist._id}>
+          <td className='table__cell'>
+            { artistBioUrl }
+          </td>
+          <td className="table__cell">
+            {artist.location.city}
+          </td>
+          <td className="table__cell">
+              {artistCategories}
+          </td>
+          <td className="table__cell">
+            {artist.youtube.clipExampleUrl.length > 0 &&
+              <a href={artist.youtube.clipExampleUrl}>Link</a>
+            }
+          </td>
+          <td className="table__cell table__cell--publication-date">
+            {moment(artist.createDate).format('MM/DD/YYYY HH:mm')}
+          </td>
+          <td className={artist.published !== true ? 'table__cell unpublished' : 'table__cell'}>
+            {String(artist.published)}
+          </td>
+          <td className="table__cell table__cell--action">
+            <a href={'/artists/' + artist.slug + '/edit'} className="button--primary">Edit</a>
+            <a href={'/artists/' + artist.slug + '/delete'} className="button--danger">Delete</a>
+          </td>
+        </tr>
+      );
+    });
+
     return (
       <div className="admin-content">
         <a className='button--primary--md' href='/artists/create'>Create a new artist</a>
@@ -55,9 +105,11 @@ export default class Admin extends React.Component {
                 </tr>
             </thead>
 
-            <tbody className='table__body'>
-
-            </tbody>
+            {this.state.artists.length > 0 &&
+              <tbody className='table__body'>
+                {artistsResult}
+              </tbody>
+            }
         </table>
       </div>
     );
