3fe9977b8130ad826f5678ed9d7b61744181a2de
acr <hello@crucifixarnaud.com>
2017-07-28 08:38:44 +0200

    Add basic filtering option (published/not published) in admin artist list

--- app/assets/js/admin.js
+++ app/assets/js/admin.js
@@ -11,8 +11,16 @@ export default class Admin extends React.Component {
     super(props);
 
     this.state = {
-      artists: []
+      artists: [],
+      allArtist: [],
+      filters: {
+        isPublished: true,
+        isNotPublished: true
+      }
     };
+
+    this.filterArtists = this.filterArtists.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   componentWillMount() {
@@ -37,16 +45,64 @@ export default class Admin extends React.Component {
 
       this.setState({
         artists: res,
+        allArtist: res,
         artistsTotal: res.length,
       });
 
     });
   }
 
+  filterArtists() {
+
+    let filteredArtist = [];
+
+    if (this.state.filters.isPublished === true) {
+      const publishedArtist = this.state.allArtist.filter( function (artist) {
+        return artist.published === true;
+      });
+
+      filteredArtist.push(publishedArtist);
+    }
+
+    if (this.state.filters.isNotPublished === true) {
+      const notPublishedArtist = this.state.allArtist.filter( function (artist) {
+        return artist.published === false;
+      });
+
+      filteredArtist.push(notPublishedArtist);
+    }
+
+    const mergedFilteredArtist = [].concat.apply([], filteredArtist);
+
+    this.setState({
+      artists: mergedFilteredArtist
+    });
+
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    const dataFilter = target.dataset.filter;
+
+    const filters = Object.assign(this.state.filters, {
+      [dataFilter]: value
+    });
+
+    this.setState({
+      [name]: value,
+      filters: filters
+    }, () => {
+      this.filterArtists();
+    });
+
+  }
+
   render() {
 
     // Artists row
-    const artistsResult = this.state.artists.map((artist, step) => {
+    const artistsResult = this.state.artists.map((artist) => {
       // Artist Bio Url
       let artistBioUrl;
       if (artist.bio.url) {
@@ -59,6 +115,7 @@ export default class Admin extends React.Component {
         );
       }
 
+      // Artist Categories
       const artistCategories = artist.categories.map((category, step) =>
         <span key={artist._id+step}>{category}&nbsp;</span>
       );
@@ -96,6 +153,34 @@ export default class Admin extends React.Component {
     return (
       <div className="admin-content">
         <a className='button--primary--md' href='/artists/create'>Create a new artist</a>
+
+        <div className="filters-box">
+          <h3 className="filters-box__title">Filters:</h3>
+          <div className="field-group">
+            <div className="field--inline">
+              <input
+                id="filterIsPublished"
+                type="checkbox"
+                name="filterIsPublished"
+                value={this.state.filters.isPublished}
+                defaultChecked={this.state.filters.isPublished}
+                data-filter="isPublished"
+                onChange={this.handleInputChange} />
+                <label className="field__label" htmlFor="filterIsPublished">Published</label>
+            </div>
+            <div className="field--inline">
+              <input
+                id="filterIsNotPublished"
+                type="checkbox"
+                name="filterIsNotPublished"
+                value={this.state.filters.isNotPublished}
+                defaultChecked={this.state.filters.isNotPublished}
+                data-filter="isNotPublished"
+                onChange={this.handleInputChange} />
+                <label className="field__label" htmlFor="filterIsNotPublished">Not Published</label>
+            </div>
+          </div>
+        </div>
 
         <table className='table table--artist'>
             <thead className='table__head'>
