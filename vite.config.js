d65282e39f0b3291147179badf50fab35be248ba
acr <dev@crucifixarnaud.com>
2026-03-19 10:32:33 +0100

    Build scss with vite

--- vite.config.js
+++ vite.config.js
@@ -12,11 +12,21 @@ export default defineConfig({
       input: {
         atlas: path.resolve(__dirname, 'app/assets/js/atlas.jsx'),
         admin: path.resolve(__dirname, 'app/assets/js/admin.jsx'),
+        atlas_css: path.resolve(__dirname, 'app/assets/scss/atlas.scss'),
+        admin_css: path.resolve(__dirname, 'app/assets/scss/admin.scss'),
       },
       output: {
-        entryFileNames: 'js/[name].bundle.js',
+        entryFileNames: (chunk) => {
+          if (chunk.name.endsWith('_css')) return 'css/[name].css'
+          return 'js/[name].bundle.js'
+        },
         chunkFileNames: 'js/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return `css/${assetInfo.name.replace('_css.css','.css')}`
+          }
+          return 'assets/[name].[hash][extname]'
+        },
       },
     },
   },
